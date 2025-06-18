226a10797f1d4290d9dc8374925f0c1d3479d325
ptkehoe <pattytflow@gmail.com>
2025-06-18 15:05:59 -0500

    changes

--- Start/Strings/Manipulation/Program.cs
+++ Start/Strings/Manipulation/Program.cs
@@ -7,31 +7,46 @@ string str3 = "THIS is a STRING";
 string[] strs = {"one", "two", "three", "four"};
 
 // TODO: Length of a string 
+// Console.WriteLine(str1.Length); 
 
 
-// TODO: Access individual characters
+// // TODO: Access individual characters
+// Console.WriteLine(str1[14]);
 
-
-// TODO: Iterate over a string like any other sequence of values
-
+// // TODO: Iterate over a string like any other sequence of values
+// foreach (char c in str1)
+// {
+//     Console.Write(c);
+//     if (c == 'b')
+//     {
+//         Console.WriteLine();
+//         break;
+//     }
+// }
 
 // TODO: String Concatenation         
-string outstr;
+// string outstr;
+// outstr = String.Concat(strs);// this is the array of strings
 
-
-// TODO: Joining strings together with Join
-
+// // TODO: Joining strings together with Join
+// outstr = String.Join('.', strs);
+// Console.WriteLine(outstr);
+// outstr = String.Join("---", strs);
+// Console.WriteLine(outstr);
 
 // TODO: String Comparison
 
-// Equals just returns a regular Boolean
+// // Equals just returns a regular Boolean
 // bool isEqual = str2.Equals(str3);
-// Console.WriteLine($"{isEqual}");
+// Console.WriteLine($"{isEqual}"); //false bc they are not the same
 
-// Compare will perform an ordinal comparison and return:
-// < 0 : first string comes before second in sort order
-// 0 : first and second strings are same position in sort order
-// > 0 : first string comes after the second in sort order
-
+// // Compare will perform an ordinal comparison and return:
+// // < 0 : first string comes before second in sort order
+// // 0 : first and second strings are same position in sort order
+// // > 0 : first string comes after the second in sort order
+// int result = String.Compare(str2, "This is a string");
+// Console.WriteLine($"{result}"); //0 bc they are the same
 
 // TODO: Replacing content
+string outstr = str1.Replace("fox", "cat"); //replacing fox with cat
+Console.WriteLine(outstr); //The quick brown cat jumps over the lazy dog.
--- Start/Strings/Searching/Program.cs
+++ Start/Strings/Searching/Program.cs
@@ -4,16 +4,23 @@
 string teststr = "The quick brown Fox jumps over the lazy Dog";
 
 // TODO: Contains determines whether a string contains certain content
+// Console.WriteLine($"{teststr.Contains("fox")}"); // false
+// Console.WriteLine($"{teststr.Contains("fox", StringComparison.CurrentCultureIgnoreCase)}"); // true
 
-
-// TODO: StartsWith and EndsWith determine if a string starts 
-// or ends with a given test string
-
+// // TODO: StartsWith and EndsWith determine if a string starts 
+// // or ends with a given test string
+// Console.WriteLine($"{teststr.StartsWith("The")}"); // true
+// Console.WriteLine($"{teststr.StartsWith("the", StringComparison.CurrentCultureIgnoreCase)}");
+// Console.WriteLine($"{teststr.StartsWith("dog")}"); // false
 
 // TODO: IndexOf, LastIndexOf finds the index of a substring
-
+// Console.WriteLine($"{teststr.IndexOf("the")}"); // lowercase version at 31
 
 // TODO: Determining empty, null, or whitespace
 string str1 = null;
 string str2 = "   ";
 string str3 = String.Empty;
+
+// Console.WriteLine($"{String.IsNullOrEmpty(str1)}"); // true
+// Console.WriteLine($"{String.IsNullOrEmpty(str3)}"); // false
+// Console.WriteLine($"{String.IsNullOrWhiteSpace(str2)}"); // true 
